ffadefefeea66ca2ebd2f72ae68b00e16937d57f
mariaesilva <73125564+mariaesilva@users.noreply.github.com>
2020-10-19 15:28:19 -0300

    Update main.kt

--- src/main.kt
+++ src/main.kt
@@ -6,27 +6,69 @@ fun main(){
 
     try {
         digitalHouse.registrarCurso("Desenvolvimento Mobile Iphone",2,35)
-        digitalHouse.registrarProfessorAdjunto("Joao","Neves",2,87)
-        digitalHouse.registrarProfessorTitular("Carlos","Silva",2,"Iphone")
-        digitalHouse.registrarAluno("Maria","Silva",3)
-        digitalHouse.matricularAluno(3,2)
-        digitalHouse.alocarProfessores(2,2,2)
-
     } catch (ex: Exception){
         println("Houve um problema ao cadastrar - erro $ex")
     }
+    
+    try {        
+        digitalHouse.registrarProfessorAdjunto("Joao","Neves",2,87)
+    } catch (ex: Exception){
+        println("Houve um problema ao cadastrar - erro $ex")
+    }
+    
+    try {        
+        digitalHouse.registrarProfessorTitular("Carlos","Silva",2,"Iphone")
+    } catch (ex: Exception){
+        println("Houve um problema ao cadastrar - erro $ex")
+    }
+    
+    try {        
+        digitalHouse.registrarAluno("Maria","Silva",3)
+    } catch (ex: Exception){
+        println("Houve um problema ao cadastrar - erro $ex")
+    }
+    
+    try {        
+        digitalHouse.matricularAluno(3,2)
+    } catch (ex: Exception){
+        println("Houve um problema ao cadastrar - erro $ex")
+    }
+    
+    try {        
+        digitalHouse.alocarProfessores(2,2,2)
+    } catch (ex: Exception){
+        println("Houve um problema ao cadastrar - erro $ex")
+    }
+    
+    
 
     try {
-        digitalHouse.excluirCurso(1)
-        digitalHouse.excluirProfessorAdjunto(1)
+        digitalHouse.excluirCurso(1)      
+        
+    } catch (ex: Exception){
+        println("Houve um problema ao excluir - erro $ex")
+    }
+    
+     try {
+         digitalHouse.excluirProfessorAdjunto(1)
+    } catch (ex: Exception){
+        println("Houve um problema ao excluir - erro $ex")
+    }
+    
+    try {
         digitalHouse.excluirProfessorTitular(1)
     } catch (ex: Exception){
         println("Houve um problema ao excluir - erro $ex")
     }
-
+   
 
 
 }
+
+        
+        
+        
+        
 
 
 
